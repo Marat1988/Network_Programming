fd3e0ba1bd350dc3c4469cd296c460ef59933d1c
Marat <pirat03071988@mail.ru>
2023-02-23 23:01:38 +0200

    UPDATE

---
+++ HomeWork1-2/Task3/Client/FormMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Client
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSengMsg = new System.Windows.Forms.Button();
+            this.textBoxSendMsg = new System.Windows.Forms.TextBox();
+            this.labelSendMsg = new System.Windows.Forms.Label();
+            this.textBoxLogMsg = new System.Windows.Forms.TextBox();
+            this.labelLogMsg = new System.Windows.Forms.Label();
+            this.buttonBeginListen = new System.Windows.Forms.Button();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.textBoxIPAdress = new System.Windows.Forms.TextBox();
+            this.labelIPAdress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSengMsg
+            // 
+            this.buttonSengMsg.Location = new System.Drawing.Point(325, 235);
+            this.buttonSengMsg.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSengMsg.Name = "buttonSengMsg";
+            this.buttonSengMsg.Size = new System.Drawing.Size(110, 43);
+            this.buttonSengMsg.TabIndex = 29;
+            this.buttonSengMsg.Text = "Отправить сообщение";
+            this.buttonSengMsg.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSendMsg
+            // 
+            this.textBoxSendMsg.Location = new System.Drawing.Point(137, 247);
+            this.textBoxSendMsg.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxSendMsg.Name = "textBoxSendMsg";
+            this.textBoxSendMsg.Size = new System.Drawing.Size(175, 20);
+            this.textBoxSendMsg.TabIndex = 28;
+            // 
+            // labelSendMsg
+            // 
+            this.labelSendMsg.AutoSize = true;
+            this.labelSendMsg.Location = new System.Drawing.Point(24, 247);
+            this.labelSendMsg.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSendMsg.Name = "labelSendMsg";
+            this.labelSendMsg.Size = new System.Drawing.Size(112, 13);
+            this.labelSendMsg.TabIndex = 27;
+            this.labelSendMsg.Text = "Введите сообщение:";
+            // 
+            // textBoxLogMsg
+            // 
+            this.textBoxLogMsg.Location = new System.Drawing.Point(122, 52);
+            this.textBoxLogMsg.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxLogMsg.Multiline = true;
+            this.textBoxLogMsg.Name = "textBoxLogMsg";
+            this.textBoxLogMsg.Size = new System.Drawing.Size(313, 173);
+            this.textBoxLogMsg.TabIndex = 26;
+            // 
+            // labelLogMsg
+            // 
+            this.labelLogMsg.AutoSize = true;
+            this.labelLogMsg.Location = new System.Drawing.Point(24, 54);
+            this.labelLogMsg.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelLogMsg.Name = "labelLogMsg";
+            this.labelLogMsg.Size = new System.Drawing.Size(89, 13);
+            this.labelLogMsg.TabIndex = 25;
+            this.labelLogMsg.Text = "Лог сообщений:";
+            // 
+            // buttonBeginListen
+            // 
+            this.buttonBeginListen.Location = new System.Drawing.Point(277, 13);
+            this.buttonBeginListen.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonBeginListen.Name = "buttonBeginListen";
+            this.buttonBeginListen.Size = new System.Drawing.Size(158, 31);
+            this.buttonBeginListen.TabIndex = 24;
+            this.buttonBeginListen.Text = "Подключиться к серверу";
+            this.buttonBeginListen.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(185, 19);
+            this.textBoxPort.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(76, 20);
+            this.textBoxPort.TabIndex = 23;
+            this.textBoxPort.Text = "1234";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(135, 21);
+            this.labelPort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(40, 13);
+            this.labelPort.TabIndex = 22;
+            this.labelPort.Text = "PORT:";
+            // 
+            // textBoxIPAdress
+            // 
+            this.textBoxIPAdress.Location = new System.Drawing.Point(47, 19);
+            this.textBoxIPAdress.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxIPAdress.Name = "textBoxIPAdress";
+            this.textBoxIPAdress.Size = new System.Drawing.Size(76, 20);
+            this.textBoxIPAdress.TabIndex = 21;
+            this.textBoxIPAdress.Text = "127.0.0.1";
+            // 
+            // labelIPAdress
+            // 
+            this.labelIPAdress.AutoSize = true;
+            this.labelIPAdress.Location = new System.Drawing.Point(24, 21);
+            this.labelIPAdress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelIPAdress.Name = "labelIPAdress";
+            this.labelIPAdress.Size = new System.Drawing.Size(20, 13);
+            this.labelIPAdress.TabIndex = 20;
+            this.labelIPAdress.Text = "IP:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 318);
+            this.Controls.Add(this.buttonSengMsg);
+            this.Controls.Add(this.textBoxSendMsg);
+            this.Controls.Add(this.labelSendMsg);
+            this.Controls.Add(this.textBoxLogMsg);
+            this.Controls.Add(this.labelLogMsg);
+            this.Controls.Add(this.buttonBeginListen);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.textBoxIPAdress);
+            this.Controls.Add(this.labelIPAdress);
+            this.Name = "FormMain";
+            this.Text = "Клиент";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSengMsg;
+        private System.Windows.Forms.TextBox textBoxSendMsg;
+        private System.Windows.Forms.Label labelSendMsg;
+        private System.Windows.Forms.TextBox textBoxLogMsg;
+        private System.Windows.Forms.Label labelLogMsg;
+        private System.Windows.Forms.Button buttonBeginListen;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.TextBox textBoxIPAdress;
+        private System.Windows.Forms.Label labelIPAdress;
+    }
+}
+
---
+++ HomeWork1-2/Task3/Server/FormMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class FormMain : Form
+    {
+        Thread threadWatch = null; //Поток, отвечающий за мониторинг клиента
+        Socket socketWatch = null; //Сокет, отвечающий за пониторинг клиента
+        Socket socConnection = null; //Создаем сокет, отвечающий за связь с клиентом
+
+        public FormMain()
+        {
+            InitializeComponent();
+            //Закрываем проверку на недопустимую операцию потока в текстовом поле
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            buttonBeginStartServer.Click += ButtonBeginStartServer_Click;
+            buttonSengMsg.Click += ButtonSengMsg_Click;
+        }
+
+        private void ButtonBeginStartServer_Click(object sender, EventArgs e)
+        {
+            //Определяем сокет для отслеживания информации, отправляемой клиентом, включая 3 параметра (протокол адресации IP4, потоковое соединение, протокол TCP)
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Серверу нужен IP-адрес и номер порта для отправки информации
+            IPAddress iPAddress = IPAddress.Parse(textBoxIPAdress.Text.Trim()); //Получить IP-адрес, введенный в текстовое поле
+            //Привязываем ip-адрес и номер порта к конечной точке сетевого узла
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, int.Parse(textBoxPort.Text.Trim())); //получить номер порта, введенный в текстовое поле
+            //Отслеживаем привязанный сетевой узел
+            socketWatch.Bind(endPoint);
+            //Ограничиваем длину очереди прослушивания сокета до 20
+            socketWatch.Listen(20);
+            //Создаем поток слушателя
+            threadWatch = new Thread(WatchConnecting);
+            //Устанавливаем поток формы для синхронизации с фоном
+            threadWatch.IsBackground = true;
+            //Запускаем поток
+            threadWatch.Start();
+            //После запуска потока в текстовое поле отображаем соответствующее приглашение
+            textBoxLogMsg.AppendText("Начать слушать информацию от клиента!" + "\r\n");
+        }
+        /// <summary>
+        /// Слушаем запрос от клиента
+        /// </summary>
+        private void WatchConnecting()
+        {
+            while (true) //Постоянно отслеживать запросы от клиента
+            {
+                socConnection = socketWatch.Accept();
+                textBoxLogMsg.AppendText("Клиентское соединение успешно" + "\r\n");
+                //Создаем коммуникационный поток
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(ServerRecMsg);
+                Thread thr = new Thread(pts);
+                thr.IsBackground = true;
+                thr.Start(socConnection); //Запускаем поток
+            }
+        }
+        /// <summary>
+        /// Способ отправки информации клиенту
+        /// </summary>
+        /// <param name="sendMsg">Информация об отправленной строке</param>
+        private void ServerSendMsg(string sendMsg)
+        {
+            //Преобразуем входную строку в массив байтов, который может распознать машина
+            byte[] arrSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+            //Отправляем клиенту информацию о байтовом массиве
+            socConnection.Send(arrSendMsg);
+            //Присоединяем отправленную строковую информацию к текстовому полю
+            textBoxLogMsg.AppendText("Сервер:\nВ " + DateTime.Now + " отправил " + sendMsg + "\r\n");
+        }
+
+        /// <summary>
+        /// Получение инофрмации от клиента
+        /// </summary>
+        /// <param name="socketClientPara">Объект клиентского сокета</param>
+        private void ServerRecMsg(object socketClientPara)
+        {
+            Socket socketServer = socketClientPara as Socket;
+            while (true)
+            {
+                //Создаем буфер памяти размером 1024X1024 байта, что составляет 1М
+                byte[] arrServerRecMsg = new byte[1024 * 1024];
+                int length = socketServer.Receive(arrServerRecMsg);
+                //Преобразуем байтовый массив, полученный машиной в строку, которая может быть прочитана людьми
+                string strSRecMsg = Encoding.UTF8.GetString(arrServerRecMsg, 0, length);
+                //Присоединяем отправленную строковую информаци к текстовому полю
+                textBoxLogMsg.AppendText("Клиент:\nВ " + DateTime.Now + " отправил " + strSRecMsg + "\r\n");
+            }
+        }
+        //Отправляем информацию клиенту
+        private void ButtonSengMsg_Click(object sender, EventArgs e)
+        {
+            //Вызов метода для отправки инофрмации клиенту
+            ServerSendMsg(textBoxSendMsg.Text.Trim());
+        }
+    }
+}
