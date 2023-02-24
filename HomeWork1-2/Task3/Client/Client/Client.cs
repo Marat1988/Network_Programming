454cda9eea5b0917e69b93ceba20ade86d913bdf
Marat <108996479+Marat1988@users.noreply.github.com>
2023-02-24 15:05:31 +0200

    UPDATE

--- HomeWork1-2/Task3/Client/Client/Client.cs
+++ HomeWork1-2/Task3/Client/Client/Client.cs
@@ -6,6 +6,7 @@ using System.Threading;
 
 namespace Client
 {
+
     public class Client
     {
         private Socket socketClient = null;
--- HomeWork1-2/Task3/Server/Server/Server.cs
+++ HomeWork1-2/Task3/Server/Server/Server.cs
@@ -6,6 +6,7 @@ using System.Threading;
 
 namespace Server
 {
+
     public class Server
     {
         private Thread threadWatch = null; //Поток, отвечающий за мониторинг клиента
