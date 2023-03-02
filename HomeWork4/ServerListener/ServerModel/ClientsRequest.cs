07b76180ecac38dc2dcf2e96fa6d050f5f44c9b2
Marat <pirat03071988@mail.ru>
2023-03-03 00:28:05 +0200

    UPDATE

---
+++ HomeWork4/ServerListener/ServerModel/ClientsRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ServerListener.ServerModel
+{
+    public class ClientsRequest
+    {
+        public EndPoint EndPoint { get; set; } //Клиент
+        public List<DateTime> Request { get; set; } //Время запросов
+    }
+}
--- HomeWork4/ServerListener/ServerModel/Server.cs
+++ HomeWork4/ServerListener/ServerModel/Server.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -15,6 +17,8 @@ namespace ServerListener.ServerModel
         private string ipAdress;
         private int port;
         public Image caesarSalad { get; set; }
+        private List<ClientsRequest> clientsRequests = new List<ClientsRequest>(); //Список для хранения запросов клиентов
+
         public Server(string ipAdress, int port)
         {
             this.ipAdress = ipAdress;
@@ -55,19 +59,43 @@ namespace ServerListener.ServerModel
             {
                 while (true)
                 {
+                    UnlockClient();
+                    ClientsRequest client = null;
                     byte[] data = new byte[1024];
                     IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
                     EndPoint Remote = (EndPoint)(sender);
                     int length = udpSocket.ReceiveFrom(data, ref Remote);
                     string messageClient = Encoding.UTF8.GetString(data, 0, length);
                     InfoMessage("Сервер: В " + DateTime.Now + " от " + Remote.ToString() + " получена строка: " + messageClient);
-                    UdpClient client = new UdpClient();
-                    sendMessage(client, (IPEndPoint)Remote, Products.getRecipe(messageClient));
-                    if (messageClient == "Салат Цезарь") //Отправка изображения
+                    client = clientsRequests.Find(item => item.EndPoint.ToString() == Remote.ToString());
+                    if (client == null)
                     {
-                        sendImage((IPEndPoint)Remote);
+                        client = new ClientsRequest
+                        {
+                            EndPoint = Remote,
+                            Request = new List<DateTime>()
+                        };
+                        client.Request.Add(DateTime.Now);
+                        clientsRequests.Add(client);
                     }
-
+                    else
+                    {
+                        client.Request.Add(DateTime.Now);
+                    }
+                    //Узнаем количество запросов клиента за последний час
+                    int countRequestClient = client.Request.Where(p => p > DateTime.Now.AddHours(-1)).ToList().Count;
+                    if (countRequestClient > 10)
+                    {
+                        sendMessage((IPEndPoint)Remote, "Вы превысили максимальное количество запросов за последний час!");
+                    }
+                    else
+                    {
+                        sendMessage((IPEndPoint)Remote, Products.getRecipe(messageClient));
+                        if (messageClient == "Салат Цезарь") //Отправка изображения
+                        {
+                            sendImage((IPEndPoint)Remote);
+                        }
+                    }
                 }
             }
             catch (SocketException ex)
@@ -79,7 +107,19 @@ namespace ServerListener.ServerModel
                 InfoMessage("Ошибка: " + ex.Message);
             }
         }
+        /// <summary>
+        /// Процедура отключения клиента, если не активен в течении 10 минут.
+        /// </summary>
+        private void UnlockClient()
+        {
+            //В данном случае просто удаление из списка
+            clientsRequests.RemoveAll(item => item.Request[item.Request.Count - 1] < DateTime.Now.AddMinutes(-2));
+        }
 
+        /// <summary>
+        /// Отправка изображения
+        /// </summary>
+        /// <param name="iPEndPoint">Удаленный IP-адрес клиента</param>
         private void sendImage(IPEndPoint iPEndPoint)
         {
             try
@@ -106,16 +146,15 @@ namespace ServerListener.ServerModel
         /// <summary>
         /// Метод отправки сообщения клиенту
         /// </summary>
-        /// <param name="udpClient">UdpClient для отправки сообщения</param>
         /// <param name=iPEndPoint">Удаоенный порт клиента</param>
         /// <param name="message">Сообщение</param>
-        private void sendMessage(UdpClient udpClient, IPEndPoint iPEndPoint, string message)
+        private void sendMessage(IPEndPoint iPEndPoint, string message)
         {
             try
             {
                 byte[] bytes = Encoding.UTF8.GetBytes(message);
-                udpClient.Send(bytes, bytes.Length, iPEndPoint);
-                InfoMessage("Сервер: В " + DateTime.Now + iPEndPoint.ToString() + " отправил сообщение: " + message);
+                udpSocket.SendTo(bytes, bytes.Length, SocketFlags.None, iPEndPoint);
+                InfoMessage("Сервер: В " + DateTime.Now + " клиенту " + iPEndPoint.ToString() + " отправил сообщение: " + message);
             }
             catch (SocketException ex)
             {
